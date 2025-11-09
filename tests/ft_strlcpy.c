f72e833be8d8e227400944767b32ffa793a6d5fd
leonardecavele <leonard.decavele@gmail.com>
2025-11-09 15:11:31 +0100

    now handles segfaults as a test

--- tests/ft_strlcpy.c
+++ tests/ft_strlcpy.c
@@ -6,37 +6,47 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 10:50:59 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/09 12:01:37 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/11/09 15:11:09 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tester.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+size_t strlcpy(char *dest, const char *src, size_t size);
 
 int	main(void)
 {
-	signal(SIGSEGV, &segfault_handler);
-
-	char	*ft_dest;
-	char	*dest;
-	char	*src;
-	int		size;
+	size_t	ft_res, res, size;
+	char	*src, *dest, *ft_dest;
+    int		status;
 
 	// test 1
 	set_description("Test regular string");
 	ft_dest = malloc(10); dest = malloc(10);
-	src = "Hello world";
-	size = 5;
-	check_is_equal(SIZE_T,
-	ft_strlcpy(ft_dest, src, size),
-	strlcpy(dest, src, size));
+	src = "Hello world"; size = 5;
+	ft_res = ft_strlcpy(ft_dest, src, size); res = strlcpy(dest, src, size);
+	check_is_equal(SIZE_T, ft_res, res);
 
+	// test 2
 	set_description("Empty dest");
-	ft_dest = ""; dest = "";
-	src = "Hello";
-	size = 5;
-	check_is_equal(SIZE_T,
-	ft_strlcpy(ft_dest, src, size),
-	strlcpy(dest, src, size));
-	
+	ft_dest = "";
+	src = "Hello"; size = 2;
+	pid_t pid = fork();
+	if (pid == 0)
+	{
+		(void)ft_strlcpy(ft_dest, src, size);
+		exit(0);
+	}
+	waitpid(pid, &status, 0);
+	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGSEGV)
+		check_is_equal(STR, "Segfault", "Segfault");
+	else
+		check_is_equal(STR, "No Segfault", "Segfault");
+
+	// test 3
 	return (0);
 }
