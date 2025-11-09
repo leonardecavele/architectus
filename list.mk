7074f2a5b7eddad11f3e02e0432252adcf52c01b
leonardecavele <leonard.decavele@gmail.com>
2025-11-10 00:29:38 +0100

    added bonuses

--- list.mk
+++ list.mk
@@ -36,15 +36,15 @@ TESTS = \
 	ft_putendl_fd.c \
 	ft_putnbr_fd.c
 
-#BTESTS = \
-#	ft_lstadd_back_bonus.c \
-#	ft_lstadd_front_bonus.c \
-#	ft_lstlast_bonus.c \
-#	ft_lstnew_bonus.c \
-#	ft_lstsize_bonus.c \
-#	ft_lstdelone_bonus.c \
-#	ft_lstclear_bonus.c \
-#	ft_lstiter_bonus.c \
-#	ft_lstmap_bonus.c
+BTESTS = \
+	ft_lstadd_back_bonus.c \
+	ft_lstadd_front_bonus.c \
+	ft_lstlast_bonus.c \
+	ft_lstnew_bonus.c \
+	ft_lstsize_bonus.c \
+	ft_lstdelone_bonus.c \
+	ft_lstclear_bonus.c \
+	ft_lstiter_bonus.c \
+	ft_lstmap_bonus.c
 
 SRCS = tester.c
